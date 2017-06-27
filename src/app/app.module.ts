4eb33943c171433499544957d7b1d3301d1f00ff
Ghita Marian POP <Ghita-Marian.POP@ext.ec.europa.eu>
2017-06-27 10:25:29 +0200

    added fw module to app module (first commit)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpaModule } from "../fw/fw.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SpaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
